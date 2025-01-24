49205f5c769f661d150cf5ef52a6d70be61f2443
phronesistechgit <phronesistechgit@gmail.com>
2025-01-24 19:39:49 +0530

    removed bg image

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,17 +2,13 @@ import React from "react";
 
 const HomePage = () => {
 	return (
-		<section
-			className="p-16 bg-cover bg-center text-center"
-			style={{
-				backgroundImage: "url('hero.jpg')",
-			}}>
+		<section className="p-16 bg-cover bg-center text-center bg-[#007CC2]">
 			<h1
 				className="mt-5 text-5xl font-bold drop-shadow-[0_2px_2px_rgba(0,0,0,1)]
- text-blue-600">
+ text-blue-400">
 				Phronesis Tech
 			</h1>
-			<h1 className="text-2xl mt-2 font-bold drop-shadow-[0_2px_2px_rgba(0,0,0,1)] text-blue-600">
+			<h1 className="text-2xl mt-2 font-bold drop-shadow-[0_2px_2px_rgba(0,0,0,1)] text-blue-400">
 				PIONEERING EXCELLENCE, ACHIEVING RESULTS
 			</h1>
 			<p className="text-white mt-4 text-lg">
