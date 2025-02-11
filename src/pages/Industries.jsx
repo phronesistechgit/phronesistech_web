21c48cebe0f9df135f777d626cee5d4893f2bb55
phronesistechgit <phronesistechgit@gmail.com>
2025-02-11 22:10:50 +0530

    changed padding for Industries

--- src/pages/Industries.jsx
+++ src/pages/Industries.jsx
@@ -6,58 +6,56 @@ import logistics from "../assets/logistics.jpg";
 import tech from "../assets/tech.jpg";
 
 const Industries = () => {
-  return (
-    <section
-      id="industries"
-      className="min-h-screen py-16 bg-blue-50 flex flex-col items-center justify-center"
-    >
-      <h2 className="text-4xl font-bold text-center text-gray-700 mb-8">
-        Industries We Serve
-      </h2>
-      <div className="max-w-7xl p-6 mx-auto flex flex-wrap justify-center gap-10">
-        {industries.map((industry, index) => (
-          <div
-            key={index}
-            className="relative p-6 rounded-2xl shadow-xl h-80 flex flex-col items-center justify-end bg-cover bg-center transition-transform transform hover:scale-105 hover:shadow-2xl overflow-hidden w-80"
-          >
-            <img
-              src={industry.image}
-              alt={industry.name}
-              className="absolute inset-0 w-full h-full object-cover opacity-80"
-            />
-            <div className="relative bg-white bg-opacity-80 p-4 w-full text-center rounded-b-2xl">
-              <h3 className="text-xl font-bold text-blue-700 z-10 drop-shadow-lg">
-                {industry.name}
-              </h3>
-            </div>
-          </div>
-        ))}
-      </div>
-    </section>
-  );
+	return (
+		<section
+			id="industries"
+			className="min-h-screen py-16 bg-blue-50 flex flex-col items-center justify-center">
+			<h2 className="text-4xl font-bold text-center text-gray-700 mb-8">
+				Industries We Serve
+			</h2>
+			<div className="max-w-7xl p-6 mx-auto flex flex-wrap justify-center gap-10">
+				{industries.map((industry, index) => (
+					<div
+						key={index}
+						className="relative p-4 rounded-2xl shadow-xl h-80 flex flex-col items-center justify-end bg-cover bg-center transition-transform transform hover:scale-105 hover:shadow-2xl overflow-hidden w-80">
+						<img
+							src={industry.image}
+							alt={industry.name}
+							className="absolute inset-0 w-full h-full object-cover opacity-80"
+						/>
+						<div className="relative bg-white bg-opacity-80 p-4 w-full text-center rounded-b-2xl">
+							<h3 className="text-xl font-bold text-blue-700 z-10 drop-shadow-lg">
+								{industry.name}
+							</h3>
+						</div>
+					</div>
+				))}
+			</div>
+		</section>
+	);
 };
 
 const industries = [
-  {
-    name: "Healthcare & Life Sciences",
-    image: healthcare,
-  },
-  {
-    name: "Retail & E-commerce",
-    image: retail,
-  },
-  {
-    name: "Supply Chain & Logistics",
-    image: logistics,
-  },
-  {
-    name: "Financial Services",
-    image: finance,
-  },
-  {
-    name: "Tech-Focused Sector",
-    image: tech,
-  },
+	{
+		name: "Healthcare & Life Sciences",
+		image: healthcare,
+	},
+	{
+		name: "Retail & E-commerce",
+		image: retail,
+	},
+	{
+		name: "Supply Chain & Logistics",
+		image: logistics,
+	},
+	{
+		name: "Financial Services",
+		image: finance,
+	},
+	{
+		name: "Tech-Focused Sector",
+		image: tech,
+	},
 ];
 
 export default Industries;
